d80acbf953cdf3a672e29ba08a73fb96758768e9
aleru3039 <aleru3039@gmail.com>
2022-12-12 06:06:16 -0800

    task

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -6,14 +6,14 @@
  *
  * Return: Always 0 (Success)
  */
+
 int main(void)
 {
-	char lower = 'a';
-	while lower <= 'z'
-	{
-		putchar(lower);
-		lower++;
-	}
-		putchar('\n');
+	char low;
+for (low = 'a'; low <= 'z'; low++)
+putchar(low);
+for (low = 'A'; low <= 'Z'; low++)
+putchar(low);
+putchar('\n');
 return (0);
 }
