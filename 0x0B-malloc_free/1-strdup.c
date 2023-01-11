e5600e767220f41ed8b311f5b3207a9f14085ff5
aleru3039 <aleru3039@gmail.com>
2023-01-10 23:16:05 -0800

    task1

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,37 +1,31 @@
 #include "main.h"
+#include <stdlib.h>
 /**
- * _strlen - count array
- * @s: array of elements
- * Return: 1
- */
-int _strlen(char *s)
-{
-	unsigned int i;
-
-	i = 0;
-while (s[i] != '\0') /*count character of string*/
-	{
-		i++;
-	}
-
-return (i);
-}
-
-/**
- * _strcpy - copy arrays
- * @src: array of elements
- * @dest: dest array
- * Return: dest
+ * _strdup - display a copy of a string in a pointer
+ * @str: string
+ * Return: Null or string
  */
 char *_strdup(char *str)
 {
-	char *dst;
-	unsigned int size;
-
-if (str == 0)
+	char *p;
+	unsigned int i, j;
+		if (str == NULL)
 	{
-return (NULL);
+		return (NULL);
 	}
-_strcpy(dst, str);
-return (dst);
+	for (i = 0; str[i] != '\0'; i++)
+		;
+	i++;
+	if (i < 1)
+		return (NULL);
+	p = malloc(sizeof(char) * i);
+	if (p == NULL)
+	{
+		free(p);
+		return (NULL);
+	}
+	for (j = 0; j < i; j++)
+		p[j] = str[j];
+	p[j] = '\0';
+return (p);
 }
